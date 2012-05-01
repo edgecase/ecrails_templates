88137b1541192ad62a4f8d33b0475c33d95f45f6
Adrian Mowat <adrian.mowat@gmail.com>
2012-05-01 16:21:39 +0100

    Added all sorts of stuff

--- rails_templates/ecuk_standard_app.rb
+++ rails_templates/ecuk_standard_app.rb
@@ -1,12 +1,33 @@
+app_name = FileUtils.getwd.split('/').last
+repo = "git@github.com:edgecase/#{app_name}.git"
+
 # >------------------ [ Gems ] ------------------< #
 
-gem "simple_form", {:version => "2.0.1"}
-gem "devise"
+gem "simple_form", :version => "2.0.1"
+gem "devise", :version => "2.0.4"
 
 gem_group :development, :test do
+  # Pull down this repo as a gem so we can run scripts form bin dir
   gem "ecrails_templates", :git => 'git@github.com:edgecase/ecrails_templates.git'
-  gem "rspec-rails", {:version => "2.9.0"}
+  gem "rspec-rails", :version => "2.9.0"
 end
+
+run "bundle install"
 
 # >------------------ [ Deployment ] ------------------< #
 
+deploy_file = "config/deploy.rb"
+
+say "Creating #{deploy_file}"
+run "bundle exec ecuk_generate_deployment.rb -n '#{app_name}' -r '#{repo}' > #{deploy_file}"
+
+# >------------------ [ Tidy up ] ------------------< #
+
+run 'rm public/index.html'
+
+# >------------------ [ Git ] ------------------< #
+
+git :init
+git :add => "."
+git :commit => "-m 'Created new application using #{__FILE__}'"
+git :remote => "add origin #{repo}"
