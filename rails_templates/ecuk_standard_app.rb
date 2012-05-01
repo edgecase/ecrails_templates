2d8dba77a1269f34033cb56b6ccf888e20fd51f5
Adrian Mowat <adrian.mowat@gmail.com>
2012-05-01 14:40:02 +0100

    Added this project to the standard project so it can bootstrap itself

--- rails_templates/ecuk_standard_app.rb
+++ rails_templates/ecuk_standard_app.rb
@@ -4,6 +4,7 @@ gem "simple_form", {:version => "2.0.1"}
 gem "devise"
 
 gem_group :development, :test do
+  gem "ecrails_templates", :git => 'git@github.com:edgecase/ecrails_templates.git'
   gem "rspec-rails", {:version => "2.9.0"}
 end
 
