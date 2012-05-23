844f23f94de8451432748154a75bd40ceb3671f3
Alan Gardner <urfolomeus@gmail.com>
2012-05-23 09:51:44 +0100

    Correcting URL for layout template

--- recipes/leftovers.rb
+++ recipes/leftovers.rb
@@ -1,6 +1,6 @@
 generate "controller home index"
 route "root :to => 'home#index'"
-get 'https://raw.github.com/edgecase/ecrails_templates/templates/application.html.haml', 'app/views/layouts/application.html.haml'
+get 'https://raw.github.com/edgecase/ecrails_templates/master/templates/application.html.haml', 'app/views/layouts/application.html.haml'
 run 'rm app/views/layouts/application.html.erb'
 run 'rm public/index.html'
 
