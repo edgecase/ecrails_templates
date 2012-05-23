6c3d8812fb91fec68c0e94f3431705ebd70093f9
Alan Gardner <urfolomeus@gmail.com>
2012-05-23 21:46:19 +0100

    Oops

--- recipes/leftovers.rb
+++ recipes/leftovers.rb
@@ -1,6 +1,6 @@
 after_bundler do
   get(
-    File.join(RECIPES_ROOT, 'templates', 'views', 'application.html.haml'),
+    File.join(RECIPES_ROOT, '..', 'templates', 'views', 'application.html.haml'),
     'app/views/layouts/application.html.haml'
   )
   run 'rm app/views/layouts/application.html.erb'
--- recipes/recipe_generator.rb
+++ recipes/recipe_generator.rb
@@ -1,5 +1,5 @@
 run 'mkdir lib/generators'
 get(
-  File.join(RECIPES_ROOT, 'templates', 'generators', 'recipe_generator.rb'),
+  File.join(RECIPES_ROOT, '..', 'templates', 'generators', 'recipe_generator.rb'),
   'lib/generators/recipe_generator.rb'
 )
