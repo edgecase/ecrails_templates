8f307e9309f0ef76f267e8bbf55c798ecbe3541e
Alan Gardner <urfolomeus@gmail.com>
2012-05-24 11:47:14 +0100

    Tweak

--- recipes/README.rb
+++ recipes/README.rb
@@ -1,7 +1,7 @@
 run 'rm README.rdoc'
 
 get File.join(TEMPLATES_ROOT, 'README.md'), 'README.md'
-inject_into_file 'README.md', :after => "##Technology\n" do
+inject_into_file 'README.md', :after => "## Technology\n" do
   %Q{. Ruby #{RUBY_VERSION}\n . Rails #{Rails::VERSION::STRING}\n}
 end
 
